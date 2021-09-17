83d10b0f94a416aa40d330ee85f3660020248f8d
HELIO MARCUS NERY FIDALGO <hmnff@hotmail.com>
2021-09-17 14:22:54 +0100

    Initial Commit

---
+++ Segue Desafio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Segue Desafio
+//
+//  Created by Helio Marcus Nery Fidalgo on 17/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Segue DesafioTests/Segue_DesafioTests.swift
@@ -0,0 +1,33 @@
+//
+//  Segue_DesafioTests.swift
+//  Segue DesafioTests
+//
+//  Created by Helio Marcus Nery Fidalgo on 17/09/21.
+//
+
+import XCTest
+@testable import Segue_Desafio
+
+class Segue_DesafioTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
